591821976311a714ce43a834dbb8df6c36a8cada
gathei-njoroge <60644525+gathei-iraki@users.noreply.github.com>
2024-07-24 16:35:04 +0300

    added more classes to the toggle

--- impoves.js
+++ impoves.js
@@ -2,8 +2,28 @@ $(document).ready(function(){
     $("#v-pills-products-tab").click(function(){
       $("#v-pills-register-tab").toggle();
       $("#v-pills-import-tab").toggle();
+      $("#v-pills-serial-tab").toggle();
+
+      $("#v-pills-item-tab").toggle();
+
+      $("#v-pills-map-tab").toggle();
+
+      $("#v-pills-stock-tab").toggle();
+
+      $("#v-pills-reconcile-tab").toggle();
+
+      $("#v-pills-move-tab").toggle();
+
+      $("#v-pills-requisition-tab").toggle();
+
 
     });
   });
 
   
+ 
+  
+  
+ 
+  
+ 
