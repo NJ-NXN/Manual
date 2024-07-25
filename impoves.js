7756009239883ba70ac962fbc7931167f415d063
gathei-njoroge <60644525+gathei-iraki@users.noreply.github.com>
2024-07-25 14:21:05 +0300

    changes

--- impoves.js
+++ impoves.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
   $("#v-pills-configs-tab").click(function(){
     $("#v-pills-organizationconfiguration-tab").toggle();
+    $("#v-pills-generalconfiguration-tab").toggle();
+
   });
   
   $("#v-pills-configs-tab").click(function(){
     $("#v-pills-organizationdetails-tab").hide();
     $("#v-pills-stations-tab").hide();
     $("#v-pills-departments-tab").hide();
+    $("#v-pills-happyhour-tab").hide();
+    $("#v-pills-setattributes-tab").hide();
+    $("#v-pills-itemcategories-tab").hide();
+    $("#v-pills-setbrands-tab").hide();
+    $("#v-pills-setcolors-tab").hide();
+    $("#v-pills-setunits-tab").hide();
+    $("#v-pills-taxtypes-tab").hide();
+    $("#v-pills-fiscalyears-tab").hide();
+    $("#v-pills-tablecounters-tab").hide();
+    $("#v-pills-setstores-tab").hide();
+
   });
 
 
@@ -17,7 +30,22 @@ $(document).ready(function(){
 
   });
 
+  $("#v-pills-generalconfiguration-tab").click(function(){
+    $("#v-pills-happyhour-tab").toggle();
+    $("#v-pills-setattributes-tab").toggle();
+    $("#v-pills-itemcategories-tab").toggle();
+    $("#v-pills-setbrands-tab").toggle();
+    $("#v-pills-setcolors-tab").toggle();
+    $("#v-pills-setunits-tab").toggle();
+    $("#v-pills-taxtypes-tab").toggle();
+    $("#v-pills-fiscalyears-tab").toggle();
+    $("#v-pills-tablecounters-tab").toggle();
+    $("#v-pills-setstores-tab").toggle(); 
 
+
+
+
+  });
 
 
 
@@ -49,7 +77,6 @@ $(document).ready(function(){
   });
 
   
- 
   
  
   
