4df5294b934c7c6408c6e51f5521c1b3f0c1df29
NJ-NXN <152908210+NJ-NXN@users.noreply.github.com>
2024-07-25 14:24:53 +0300

    Scroll to top

--- impoves.js
+++ impoves.js
@@ -1,34 +1,25 @@
-$(document).ready(function(){
-    $("#v-pills-products-tab").click(function(){
-      $("#v-pills-register-tab").toggle();
-      $("#v-pills-import-tab").toggle();
-      $("#v-pills-serial-tab").toggle();
+$(document).ready(function () {
+  $("#v-pills-products-tab").click(function () {
+    $("#v-pills-register-tab").toggle();
+    $("#v-pills-import-tab").toggle();
+    $("#v-pills-serial-tab").toggle();
 
-      $("#v-pills-item-tab").toggle();
+    $("#v-pills-item-tab").toggle();
 
-      $("#v-pills-map-tab").toggle();
+    $("#v-pills-map-tab").toggle();
 
-      $("#v-pills-stock-tab").toggle();
+    $("#v-pills-stock-tab").toggle();
 
-      $("#v-pills-reconcile-tab").toggle();
+    $("#v-pills-reconcile-tab").toggle();
 
-      $("#v-pills-move-tab").toggle();
+    $("#v-pills-move-tab").toggle();
 
-      $("#v-pills-requisition-tab").toggle();
+    $("#v-pills-requisition-tab").toggle();
 
-      $("#v-pills-approve-tab").toggle();
+    $("#v-pills-approve-tab").toggle();
 
-      $("#v-pills-recipe-tab").toggle();
+    $("#v-pills-recipe-tab").toggle();
 
-      $("#v-pills-wastes-tab").toggle();
-
-
-    });
+    $("#v-pills-wastes-tab").toggle();
   });
-
-  
-  
-  
- 
-  
- 
+});
