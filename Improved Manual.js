ac107b27161ac076dfdaaf43c4228b82c2494f0d
gathei-njoroge <60644525+gathei-iraki@users.noreply.github.com>
2024-07-29 14:29:44 +0300

    reversed merge changes

--- Improved Manual.js
+++ Improved Manual.js
@@ -1,22 +1,83 @@
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+$(document).ready(function(){
+  $("#v-pills-configs-tab").click(function(){
+    $("#v-pills-organizationconfiguration-tab").toggle();
+    $("#v-pills-generalconfiguration-tab").toggle();
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
+  });
+  
+  $("#v-pills-configs-tab").click(function(){
+    $("#v-pills-organizationdetails-tab").hide();
+    $("#v-pills-stations-tab").hide();
+    $("#v-pills-departments-tab").hide();
+    $("#v-pills-happyhour-tab").hide();
+    $("#v-pills-setattributes-tab").hide();
+    $("#v-pills-itemcategories-tab").hide();
+    $("#v-pills-setbrands-tab").hide();
+    $("#v-pills-setcolors-tab").hide();
+    $("#v-pills-setunits-tab").hide();
+    $("#v-pills-taxtypes-tab").hide();
+    $("#v-pills-fiscalyears-tab").hide();
+    $("#v-pills-tablecounters-tab").hide();
+    $("#v-pills-setstores-tab").hide();
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+  });
 
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+
+  $("#v-pills-organizationconfiguration-tab").click(function(){
+    $("#v-pills-organizationdetails-tab").toggle();
+    $("#v-pills-stations-tab").toggle();
+    $("#v-pills-departments-tab").toggle();
+
+  });
+
+  $("#v-pills-generalconfiguration-tab").click(function(){
+    $("#v-pills-happyhour-tab").toggle();
+    $("#v-pills-setattributes-tab").toggle();
+    $("#v-pills-itemcategories-tab").toggle();
+    $("#v-pills-setbrands-tab").toggle();
+    $("#v-pills-setcolors-tab").toggle();
+    $("#v-pills-setunits-tab").toggle();
+    $("#v-pills-taxtypes-tab").toggle();
+    $("#v-pills-fiscalyears-tab").toggle();
+    $("#v-pills-tablecounters-tab").toggle();
+    $("#v-pills-setstores-tab").toggle(); 
+
+
+
+
+  });
+
+
+
+    $("#v-pills-products-tab").click(function(){
+      $("#v-pills-register-tab").toggle();
+      $("#v-pills-import-tab").toggle();
+      $("#v-pills-serial-tab").toggle();
+
+      $("#v-pills-item-tab").toggle();
+
+      $("#v-pills-map-tab").toggle();
+
+      $("#v-pills-stock-tab").toggle();
+
+      $("#v-pills-reconcile-tab").toggle();
+
+      $("#v-pills-move-tab").toggle();
+
+      $("#v-pills-requisition-tab").toggle();
+
+      $("#v-pills-approve-tab").toggle();
+
+      $("#v-pills-recipe-tab").toggle();
+
+      $("#v-pills-wastes-tab").toggle();
+
+
+    });
+  });
+
+  
+  
+ 
+  
+ 
